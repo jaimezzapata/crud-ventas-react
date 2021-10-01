e6ec8d4350e4a5a770fca1ae3a3ae8feccc12edd
Jaime Zapata <zapataval2304@gmail.com>
2021-10-01 08:58:27 -0500

    Se crea la conección a DB y fuciona correctamente

---
+++ src/main/java/co/com/zz/crudVentas/CrudVentasApplication.java
@@ -0,0 +1,13 @@
+package co.com.zz.crudVentas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudVentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudVentasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/zz/crudVentas/CrudVentasApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.zz.crudVentas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudVentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
